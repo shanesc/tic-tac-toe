4946465dfa7e55656fb520d5766ea6b70a56f78b
Shane Mitchell <shaneSC@gmail.com>
2021-02-14 15:25:49 +0000

    feat: add controller module functionality

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const board = (function () {
   let _board = _initialize();
   //   create a method to return the board array
   //     function getboard => return board array;
-  function getBoard() {
+  function getState() {
     return _board;
   }
   //   create a method to update board array
@@ -50,53 +50,9 @@ const board = (function () {
     return _board;
   }
 
-  // create a method to determine result of board - draw, win, or null (i.e. not finished yet)
-  function getResult() {
-    //   if any null values in board, return null (not all moves made)
-    if (_board.includes(null)) {
-      return null;
-      //   else if check against 8 winning combos, if true return win (move to own function)
-    } else if (checkForWinner()) {
-      return 'win';
-      //   else return draw
-    } else {
-      return 'draw';
-    }
-
-    function checkForWinner() {
-      // deconstruct array for easier references to positions
-      const [p1, p2, p3, p4, p5, p6, p7, p8, p9] = _board;
-
-      // return true for the following
-      //     1 === 2 === 3, 4 === 5 === 6, 7 === 8 === 9,
-      //     1 === 4 === 7, 2 === 5 === 8, 3 === 6 === 9,
-      //     1 === 5 === 9, 3 === 5 === 7
-      if (new Set([p1, p2, p3]).size === 1) {
-        return true;
-      } else if (new Set([p4, p5, p6]).size === 1) {
-        return true;
-      } else if (new Set([p7, p8, p9]).size === 1) {
-        return true;
-      } else if (new Set([p1, p4, p7]).size === 1) {
-        return true;
-      } else if (new Set([p2, p5, p8]).size === 1) {
-        return true;
-      } else if (new Set([p3, p6, p9]).size === 1) {
-        return true;
-      } else if (new Set([p1, p5, p9]).size === 1) {
-        return true;
-      } else if (new Set([p3, p5, p7]).size === 1) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  }
-
   return {
-    getBoard,
+    getState,
     update,
-    getResult,
     clear,
   };
 })();
@@ -124,14 +80,17 @@ function Player(name, marker, human = false) {
   function getMarker() {
     return marker;
   }
-  function makeSelection(board) {
+  function makeSelection(board = null) {
     // create selection variable
+    let selection;
     if (human) {
       // prompt user for input based on available board spots
+      selection = prompt('Make a selection');
     } else {
       // do some AI selection based on board
     }
     // return selection
+    return selection;
   }
 
   return {
@@ -153,22 +112,75 @@ controller (i.e. referee): controls the flow of game, divvy out marker, who make
 
 // create gameControl module
 const controller = (() => {
-  //  start
-  //    clear board
-  //    create players
-  //    prompt player1 for selection
-  function start() {}
-  //  _getGameState(board)
-  //    if game is over, return tie or winner
-  //    if game is not over, return in progress
-  //  _getPlayerSelection(player)
-  //    update board with selection
-  //  makeTurn()
-  //    switch active player
-  //    getPlayerSelection(activePlayer)
-  //  end
-  //    return winner
+  function play(playerOne, playerTwo) {
+    board.clear();
+    let activePlayer = null;
+    let keepPlaying = true;
+
+    while (keepPlaying) {
+      activePlayer =
+        activePlayer === playerOne ? playerTwo : playerOne;
+      const selection = activePlayer.makeSelection(board);
+      board.update(activePlayer.getMarker(), selection);
+      console.log(board.getState());
+      if (_isWinner() || _isDraw()) {
+        console.log('stop');
+        keepPlaying = false;
+      }
+    }
+    _end(_isWinner ? activePlayer : 'Draw');
+  }
+  function _isWinner() {
+    // deconstruct array for easier references to positions
+    const [p1, p2, p3, p4, p5, p6, p7, p8, p9] = board.getState();
+
+    // return true for the following
+    //     1 === 2 === 3, 4 === 5 === 6, 7 === 8 === 9,
+    //     1 === 4 === 7, 2 === 5 === 8, 3 === 6 === 9,
+    //     1 === 5 === 9, 3 === 5 === 7
+    if (new Set([p1, p2, p3]).size === 1 && p1 !== null) {
+      return true;
+    } else if (new Set([p4, p5, p6]).size === 1 && p4 !== null) {
+      return true;
+    } else if (new Set([p7, p8, p9]).size === 1 && p7 !== null) {
+      return true;
+    } else if (new Set([p1, p4, p7]).size === 1 && p1 !== null) {
+      return true;
+    } else if (new Set([p2, p5, p8]).size === 1 && p2 !== null) {
+      return true;
+    } else if (new Set([p3, p6, p9]).size === 1 && p3 !== null) {
+      return true;
+    } else if (new Set([p1, p5, p9]).size === 1 && p1 !== null) {
+      return true;
+    } else if (new Set([p3, p5, p7]).size === 1 && p3 !== null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function _isDraw() {
+    if (_isWinner || board.getState().includes(null)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function _end(result) {
+    alert(result.getName() || result);
+  }
+
+  return {
+    play,
+  };
 })();
+
+//  start
+
+//    create players
+const player1 = Player('test', 'X', true);
+const player2 = Player('tester', 'O', true);
 
 //   create variables to initialize players
 //     player1 = {name: foo.getName, marker: foo.getMarker, human}
